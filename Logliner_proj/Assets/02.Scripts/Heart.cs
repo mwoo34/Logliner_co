cacfdd3325df3fd4d7c9a4e45eb8e4b58559187e
minwoo <enmw34@gmail.com>
2021-09-29 12:31:05 +0900

    first commit

---
+++ Logliner_proj/Assets/02.Scripts/Heart.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Heart : MonoBehaviour
+{
+    public GameObject[] cubes;
+    public Canvas[] images;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
