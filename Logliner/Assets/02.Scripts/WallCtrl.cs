6fe336cd9f54b7dad0121f9df7e16258662f3eff
minwoo <enmw34@gmail.com>
2021-10-04 15:59:26 +0900

    add ch3 1004_3

--- Logliner/Assets/02.Scripts/WallCtrl.cs
+++ Logliner/Assets/02.Scripts/WallCtrl.cs
@@ -5,13 +5,16 @@ using UnityEngine;
 public class WallCtrl : MonoBehaviour
 {
     public GameObject[] hearts;
+    public Sprite heart;
+    public Sprite empty_heart;
     private int hp_count = 5;
 
     void OnCollisionEnter(Collision coll) {
         if (coll.collider.CompareTag("CUBE")) {
             Destroy(coll.gameObject);
             if (hp_count > 0) {
-                hearts[hp_count-1].SetActive(false);
+                //hearts[hp_count-1].SetActive(false);
+                hearts[hp_count-1].gameObject.GetComponent<SpriteRenderer>().sprite = empty_heart;
                 hp_count--;
             }
         }
